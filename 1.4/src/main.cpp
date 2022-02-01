9eda6a5fcc4b35121370373c9de36a2e7841a87d
Caglar Kilimci <ckilimci@gmail.com>
2022-02-01 17:58:22 +0300

    Question 1.4

---
+++ 1.4/src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrom(string s) {
+    // Create a frequency map
+    // Frequency should be even. Only 1 element can be odd. Ignore space!
+    int freq[128];
+    int val;
+    for (int i=0; i<128; i++) {
+        freq[i] = 0;
+    }
+    for (int i=0; i<s.length(); i++) {
+        val = s[i];
+        freq[val]++;
+    }
+    bool flag = false;
+    for (int i=0; i<128; i++) {
+        if (freq[i] % 2 != 0) {
+            val = ' ';
+            if (freq[i] == val) {       // Ignore space
+                continue;
+            } else if (flag == false) { // Set flag
+                flag = true;
+            } else {                    // Found second odd char
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(int argc, char const *argv[])
+{
+    cout<< "Question 1.4:" << endl;
+    cout<< "Check the given string whether polindrom or not." << endl;
+
+    string test="aaabcbaaa";
+    if (isPalindrom(test)) {
+        cout << test << " is polindrom!" << endl;
+    } else {
+        cout << test << " is NOT polindrom." << endl;
+    }
+
+    return 0;
+}
